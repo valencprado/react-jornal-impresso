aa9034b2e8ab853f77880034695c4c2ac25b1270
Valen <valenpschool@gmail.com>
2022-09-07 23:37:00 -0300

    feat: nova rota

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,20 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from '../pages/home';
-export default function BlogRoutes(){
-    <BrowserRouter>
+import About from '../pages/about';
+import NavBar from '../components/navbar';
+import Footer from '../components/footer';
+import Login from '../pages/login';
+
+export default function AppRoutes(){
+    return(
+        <BrowserRouter>
+    <NavBar/>
     <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/login' element={<Login />}/>
     </Routes>
+    <Footer/>
     </BrowserRouter>
+        )
 }
