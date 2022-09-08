6f94902bbe875b2588bbbd944ec9f43472167148
Valen <valenpschool@gmail.com>
2022-09-07 23:37:14 -0300

    feat: página de login

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,13 @@
+// página de login para escrever blogs
+// vou pesquisar sobre o formulário amanhã
+import './styles.css';
+export default function Login(){
+    return(
+        <div className="login-container">
+            <div>
+            <h2>Área de Escritores</h2>
+            
+            </div>
+        </div>
+    )
+}
