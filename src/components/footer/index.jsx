dd4a6d10069e117fad01268cbf2d2202b82383c8
Valen <valenpschool@gmail.com>
2022-09-07 23:36:25 -0300

    feat: rodapé

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css';
+export default function Footer(){
+    return(
+        <div className="footer-container">
+    <h4>Criado por <a href="https://valencprado.github.io/personal-website/" target='_blank'> Valenn</a> e <a href="" target='_blank'>Gus</a>.</h4>
+    <h4>Todos os direitos reservados. 2022.</h4>
+    </div>
+        )
+}
