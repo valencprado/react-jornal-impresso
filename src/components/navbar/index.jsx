995523ef0ab4f051661a05f1cc3d5cd3ae32f023
Valen <valenpschool@gmail.com>
2022-09-07 23:36:48 -0300

    feat: estilo da navbar + links

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,14 +1,15 @@
-import 'styles.css';
+import './styles.css';
 import {Link} from 'react-router-dom';
 export default function NavBar(){
     return(
         <nav className='navbar'>
             <div>Logo</div>
             <div className='links'>
-                <Link className='' to={''}>Home</Link> 
-                <Link className='' to={''}>Posts</Link> 
-                <Link className='' to={''}>Sobre</Link> 
-                <Link className='' to={''}>Escritores</Link> 
+                <Link className='link' to={'/'}>Home</Link> 
+                <Link className='link' to={'/Posts'}>Posts</Link> 
+                <Link className='link' to={'/About'}>Sobre</Link> 
+                <Link className='link' to={'/Writers'}>Escritores</Link> 
+                <Link className='writer-page' to={'/Login'}></Link>
             </div>
         </nav>
     )
