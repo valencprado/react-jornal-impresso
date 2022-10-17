1070b292379ff44fd6c3efbe695d0a6ac9a4e6b4
Valen <valenpschool@gmail.com>
2022-10-17 12:41:05 -0300

    adicionar projeto

---
+++ components/BlogCard.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import styles from '../styles/blogcard.module.css'
+import Image from 'next/image';
+import moment from 'moment';
+export default function BlogPost({title, author, date,coverPhoto, key, slug}){
+return(
+    <div className={styles.card}>
+       
+      <Link href={`/posts/${slug}`}>
+      <div className={styles.text}>
+        <h2>{title}</h2>
+        <div className={styles.details}>
+          <div className={styles.author}>
+            <img src={author.avatar.url} alt={author.nome} />
+            <h3>{author.nome}</h3>
+          </div>
+          <div className={styles.date}>
+            <h3>{moment(date).format("MMMM d, YYYY")}</h3>
+          </div>
+        </div>
+      </div>
+      </Link>
+    </div>
+);
+}
+
+//  <Image width='100%' height='100%' src={coverPhoto.url} alt="" />
---
+++ pages/posts/[slug].js
@@ -0,0 +1,77 @@
+import styles from '../../styles/slug.module.css'
+import {GraphQLClient, gql} from 'graphql-request'
+import moment from "moment";
+
+// conexão com a API
+const graphcms = new GraphQLClient("https://api-sa-east-1.hygraph.com/v2/cl9c8y4wo4g7701t6cy9f4x40/master")
+const query = gql`
+query Post($slug: String!) {
+    post(where: { slug: $slug }) {
+      id
+      titulo
+      slug
+      data
+      author {
+        id
+        nome
+        avatar {
+          url
+        }
+      }
+      conteudo {
+        html
+      }
+      fotoInicial {
+        id
+        url
+      }
+    }
+  }
+`;
+const SLUGLIST = gql`
+  {
+    posts {
+      slug
+    }
+  }
+`;
+export async function getStaticPaths(){
+    const {posts} = await graphcms.request(SLUGLIST);
+    return{
+        paths:posts.map((posts) => ({params:{slug: posts.slug}})),
+        fallback:false,
+    }
+}
+export async function getStaticProps({params}){
+    const slug = params.slug;
+  const data = await  graphcms.request(query, {slug});
+  const post = data.post;
+  return{
+    props:{
+      post,
+    },
+    revalidate: 10,
+  };
+}
+
+export default function BlogPost({post}){
+     return(
+        <main className={styles.blog}>
+            <img src={post.fotoInicial.url} className={styles.cover}/>
+            <div className={styles.title}>
+                <div className={styles.authimg}>
+                <img  src={post.author.avatar.url }/> 
+                    <h6>By {post.author.nome}</h6>
+                </div>
+                <div className={styles.authtext}>
+                    <h6 className={styles.date}>{moment(post.data).format("MMMM d, YYYY")}</h6>
+                    </div>   
+            </div>
+            <h2>{post.title}</h2>
+            <div
+        className={styles.content}
+      dangerouslySetInnerHTML={{ __html: post.conteudo.html}}
+      ></div>
+        </main>
+     )
+}
