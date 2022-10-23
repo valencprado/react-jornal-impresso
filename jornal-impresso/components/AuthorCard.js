8127ab8a9737ca2ab4b1cb50466a3981e6e0511a
Valen <valenpschool@gmail.com>
2022-10-23 20:06:28 -0300

    feat: cartão do autor (comentado)

---
+++ jornal-impresso/components/AuthorCard.js
@@ -0,0 +1,26 @@
+ import styles from '../styles/AuthorCard.module.css';
+
+
+ export default function AuthorCard({author}){
+  
+ return(
+    
+   <div className={styles.card}>
+{/*    
+       <div className={styles.imgContent}>
+           <img src={author.avatar.url} alt={author.nome} />
+        
+       </div>
+     <div className={styles.text}>
+       <h2>{author.nome}</h2>
+       <div className={styles.details}>
+         <div className={styles.author}>
+           <h3>{author.descricao}</h3>
+         </div>
+       </div>
+     </div> */}
+   </div>
+ );
+ }
+
+  
