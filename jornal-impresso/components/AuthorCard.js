c969fad5f7b2a8605eb4462add070a9b9c607914
Valen <valenpschool@gmail.com>
2022-10-30 12:22:56 -0300

    fix: dados da API (funcionando)

--- jornal-impresso/components/AuthorCard.js
+++ jornal-impresso/components/AuthorCard.js
@@ -1,25 +1,19 @@
  import styles from '../styles/AuthorCard.module.css';
+ import Image from 'next/image';
 
-
- export default function AuthorCard({author}){
+ export default function AuthorCard({avatar, nome, descricao}){
   
  return(
     
    <div className={styles.card}>
-{/*    
+  
        <div className={styles.imgContent}>
-           <img src={author.avatar.url} alt={author.nome} />
-        
+           <img src={avatar.url} alt={descricao} />
        </div>
      <div className={styles.text}>
-       <h2>{author.nome}</h2>
-       <div className={styles.details}>
-         <div className={styles.author}>
-           <h3>{author.descricao}</h3>
-         </div>
+       <h2>{nome}</h2>
        </div>
-     </div> */}
-   </div>
+     </div> 
  );
  }
 
